b65deadcd77d3c655e9ca23da0596b8252e85d95
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-08 20:31:11 +0530

    html icon

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './styles/Tab.css'
 import { RxCross1 } from 'react-icons/rx'
+import { AiFillHtml5 } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 
@@ -11,6 +12,9 @@ const Tab = ({ name }) => {
             initial={{ opacity: 0 }}
             transition={{ duration: 0.3, delay: 0.3 }}
             className="tab">
+            <div className="tab__icon">
+                <AiFillHtml5 />
+            </div>
             <div className="tab__name">
                 <span>{name}</span>.html
             </div>
