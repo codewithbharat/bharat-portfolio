0d350cd8a37a56a92f1c5c337c1c5e2ab3addec1
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-08 23:03:50 +0530

    contact form created

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import './styles/Contact.css'
 import Tab from '../components/Tab'
 import Sidebar from '../components/Sidebar'
+import { motion } from 'framer-motion'
 
 const Contact = () => {
     useEffect(() => {
@@ -13,7 +14,31 @@ const Contact = () => {
             <Sidebar brand="Contact">
                 children
             </Sidebar>
-            contact me
+            <motion.div
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                transition={{ duration: 0.3, delay: 0.3 }}
+                className="contact__page">
+                <div className="contact__page-form">
+                    <form>
+                        <label>_name:
+                            <input type="text" />
+                        </label>
+                        <label>_email:
+                            <input type="text" />
+                        </label>
+                        <label>_message:
+                            <textarea />
+                        </label>
+                        <label>
+                            <input id='submit' type="submit" value="submit-message" />
+                        </label>
+                    </form>
+                </div>
+                <div className="contact__page-code">
+                    asdfsdf
+                </div>
+            </motion.div>
         </div>
     )
 }
