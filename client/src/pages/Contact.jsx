20a394aba81998a57428a1c808c69837fc083116
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-09 02:44:01 +0530

    form_code = form_input.value

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles/Contact.css'
 import Tab from '../components/Tab'
 import Sidebar from '../components/Sidebar'
 import { motion } from 'framer-motion'
 
 const Contact = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
     useEffect(() => {
         document.title = "Contact Me";
     }, []);
+
     return (
         <div className="contact">
             <Tab name="contacts" />
@@ -22,13 +28,22 @@ const Contact = () => {
                 <div className="contact__page-form">
                     <form>
                         <label>_name:
-                            <input type="text" />
+                            <input type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
                         </label>
                         <label>_email:
-                            <input type="text" />
+                            <input type="text"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </label>
                         <label>_message:
-                            <textarea />
+                            <textarea
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                            />
                         </label>
                         <label>
                             <input id='submit' type="submit" value="submit-message" />
@@ -36,7 +51,64 @@ const Contact = () => {
                     </form>
                 </div>
                 <div className="contact__page-code">
-                    asdfsdf
+                    <p>
+                        <span className="contact__page-code__dataType">const </span>
+                        <span className="contact__page-code__syntax">button </span>
+                        <span className="contact__page-code__symbol">= </span>
+                        <span className='contact__page-code__syntax'>document.querySeclector</span>
+                        <span className="contact__page-code__symbol">{"("}</span>
+                        <span className="contact__page-code__value">'#sendBtn'</span>
+                        <span className="contact__page-code__symbol">{");"} </span>
+                    </p>
+                    <p>{" "}</p>
+                    <p>
+                        <span className="contact__page-code__dataType">const </span>
+                        <span className="contact__page-code__syntax">message </span>
+                        <span className="contact__page-code__symbol">{"= {"} </span>
+                    </p>
+                    <div>
+                        <p>
+                            <span className="contact__page-code__syntax">name</span>
+                            <span className="contact__page-code__symbol">: </span>
+                            <span className="contact__page-code__value">{'"'}<span>{name}</span>{'"'}</span>
+                            <span className="contact__page-code__symbol">, </span>
+                        </p>
+                        <p>
+                            <span className="contact__page-code__syntax">email</span>
+                            <span className="contact__page-code__symbol">: </span>
+                            <span className="contact__page-code__value">{'"'}<span>{email}</span>{'"'}</span>
+                            <span className="contact__page-code__symbol">, </span>
+                        </p>
+                        <p>
+                            <span className="contact__page-code__syntax">message</span>
+                            <span className="contact__page-code__symbol">: </span>
+                            <span className="contact__page-code__value">{'"'}<span>{message}</span>{'"'}</span>
+                        </p>
+                    </div>
+                    <p>
+                        <span className="contact__page-code__symbol"> {"}"} </span>
+                    </p>
+
+                    <p>
+                        <span className="contact__page-code__syntax">button.addeventListener</span>
+                        <span className="contact__page-code__symbol">{"("}</span>
+                        <span className="contact__page-code__value">'click</span>
+                        <span className="contact__page-code__symbol">{", () => {"}</span>
+                    </p>
+
+                    <div>
+                        <p>
+                            <span className="contact__page-code__syntax">form.send</span>
+                            <span className="contact__page-code__symbol">{"("}</span>
+                            <span className="contact__page-code__syntax">message</span>
+                            <span className="contact__page-code__symbol">{");"}</span>
+                        </p>
+
+                    </div>
+                    <p>
+                        <span className="contact__page-code__symbol">{"});"}</span>
+                    </p>
+
                 </div>
             </motion.div>
         </div>
