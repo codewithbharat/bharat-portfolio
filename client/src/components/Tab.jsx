df1a1eba2b9b4e6a87b3cd8bd2bb143e787b94c1
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-08 20:39:04 +0530

    bug Fixed

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './styles/Tab.css'
 import { RxCross1 } from 'react-icons/rx'
-import { AiFillHtml5 } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 
@@ -12,9 +11,6 @@ const Tab = ({ name }) => {
             initial={{ opacity: 0 }}
             transition={{ duration: 0.3, delay: 0.3 }}
             className="tab">
-            <div className="tab__icon">
-                <AiFillHtml5 />
-            </div>
             <div className="tab__name">
                 <span>{name}</span>.html
             </div>
