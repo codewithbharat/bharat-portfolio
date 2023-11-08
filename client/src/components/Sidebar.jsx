d1a53cfc757524746f899ec780ad1cec4b12cffd
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-08 19:11:17 +0530

    animation added

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
 import './styles/Sidebar.css'
-
 import { BiSolidDownArrow, BiSolidRightArrow } from "react-icons/bi"
+import { motion } from 'framer-motion'
 
 const Sidebar = ({ children, brand }) => {
 
     const [toggle, setToggle] = useState(true);
     return (
-        <div className="sidebar">
+        <motion.div
+            animate={{ x: 0 }}
+            initial={{ x: -300 }}
+            transition={{ duration: 0.3 }}
+            className="sidebar">
             <div className="sidebar__brand" onClick={() => setToggle(!toggle)}>
                 <span className="sidebar__brand-icon">
                     {toggle ? <BiSolidDownArrow /> : <BiSolidRightArrow />}
@@ -17,7 +21,7 @@ const Sidebar = ({ children, brand }) => {
             {toggle && <div className="sidebar__child">
                 {children}
             </div>}
-        </div>
+        </motion.div>
     )
 }
 
