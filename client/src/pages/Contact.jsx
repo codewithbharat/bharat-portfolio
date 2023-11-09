f80cb5d25f89aad8d473d85434776e420a4d3088
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-09 16:33:14 +0530

    sidebar links

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -4,6 +4,10 @@ import Tab from '../components/Tab'
 import Sidebar from '../components/Sidebar'
 import { motion } from 'framer-motion'
 
+import { GrMail } from 'react-icons/gr'
+import { BiSolidPhoneCall } from 'react-icons/bi'
+import { IoLogoWhatsapp } from 'react-icons/io'
+
 const Contact = () => {
 
     const [name, setName] = useState("");
@@ -14,11 +18,22 @@ const Contact = () => {
         document.title = "Contact Me";
     }, []);
 
+    const SidebarLink = ({ icon, name, link }) => {
+        return (
+            <div className="contact__sidebar__link">
+                <span className="contact__sidebar__link-icon"> {icon} </span>
+                <a href={link} className="contact__sidebar__link-name">{name}</a>
+            </div>
+        )
+    }
+
     return (
         <div className="contact">
             <Tab name="contacts" />
             <Sidebar brand="Contact">
-                children
+                <SidebarLink icon={<GrMail />} name="bhaarat.ranjan@gmail.com" link="mailto:bhaarat.ranjan@gmail.com" />
+                <SidebarLink icon={<BiSolidPhoneCall />} name="+91-9162601061" link="tel:+919162601061" />
+                <SidebarLink icon={<IoLogoWhatsapp />} name="+91-9162601061" link=" https://wa.me/+919162601061" />
             </Sidebar>
             <motion.div
                 animate={{ opacity: 1 }}
@@ -36,7 +51,7 @@ const Contact = () => {
                         </label>
                         <label>_email:
                             <input type="text"
-                                maxLength={20}
+                                maxLength={40}
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
