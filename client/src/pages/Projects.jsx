b1b4d2171df7a294481cfeb1e7aa52f3c64b7f80
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-09 17:28:25 +0530

    tab name fixed

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
             <Sidebar brand="Projects">
                 children
             </Sidebar>
-            <Tab name="contacts" />
+            <Tab name="projects" />
             projects
         </div>
     )
