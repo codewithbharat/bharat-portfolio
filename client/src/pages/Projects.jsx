7f0b47aef234ccf738fd34d9b33e4822f65e8ee7
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-11-10 23:53:39 +0530

    projects -> sidebarLinks

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -3,16 +3,41 @@ import './styles/Projects.css'
 import Tab from '../components/Tab'
 import Sidebar from '../components/Sidebar'
 
+import { FaReact } from 'react-icons/fa'
+import { IoLogoJavascript } from 'react-icons/io'
+import { DiNodejsSmall } from 'react-icons/di'
+import { GiSpiderWeb } from 'react-icons/gi'
+
 const Projects = () => {
     useEffect(() => {
         document.title = "Projects";
     }, []);
 
+    const LinkData = [
+        { icon: <FaReact />, name: "React" },
+        { icon: <IoLogoJavascript />, name: "JavaScript" },
+        { icon: <DiNodejsSmall />, name: "ExpressJs" },
+        { icon: <GiSpiderWeb />, name: "Full-Stack" }
 
+    ]
+
+    const SidebarLink = ({ icon, name, link }) => {
+        return (
+            <div className="projects__sidebar__link">
+                <span><input type='checkbox' className="projects__sidebar__link-checkbox" id={name} /></span>
+                <span className="projects__sidebar__link-icon"> {icon} </span>
+                <a href={link} className="projects__sidebar__link-name">{name}</a>
+            </div>
+        )
+    }
     return (
         <div className="projects">
             <Sidebar brand="Projects">
-
+                {
+                    LinkData.map(({ icon, name, }) => (
+                        <SidebarLink icon={icon} name={name} />
+                    ))
+                }
             </Sidebar>
             <Tab name="projects" />
             <div className="projects__page">
